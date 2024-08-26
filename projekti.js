99dbe9a2f86a6daed97deb57768aae28bbbadbc6
JTyniEDU <joona.tyni@edu.salpaus.fi>
2024-08-26 18:36:28 +0300

    1

---
+++ projekti.js
@@ -0,0 +1,7 @@
+function tieto() {
+if (document.getElementById("tieto1") && document.getElementById("tieto2") != null) {
+    document.write("Täytä kaikki kentät")
+} else {
+    console.log("Tieto on ok!")
+}
+}
