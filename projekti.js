9b270acf8533500313708012bb7d4af8f97f4a66
JTyniEDU <joona.tyni@edu.salpaus.fi>
2024-08-29 09:58:06 +0300

    1

--- projekti.js
+++ projekti.js
@@ -2,19 +2,23 @@ function tieto() {
   var tieto1 = document.getElementById("tieto1");
   var tieto2 = document.getElementById("tieto2");
   var tietobtn = document.getElementById("tietobtn");
-  var varoitus = document.getElementById("variotus");
+  var varoitus = document.getElementById("varoitus");
+
+  varoitus.style.visibility = "hidden";
+  varoitus.innerHTML = "";
+
   if (tieto1.value.trim() === "") {
-      console.log("Tyhjä input 1");
-      tietobtn.style.backgroundColor = "red";
-      setTimeout(function() {
-        varoitus.style.visibility = "visible";
-        varoitus.innerHTML += "Täytä kenttä 1!";
-      }, 3000 );
+    console.log("Tyhjä input 1");
+    tietobtn.style.backgroundColor = "red";
+    setTimeout(function() {
+      varoitus.style.visibility = "visible";
+      varoitus.innerHTML += "Täytä kenttä 1!";
+    }, 3000);
   } else if (tieto2.value.trim() === "") {
     console.log("Tyhjä input 2");
-    document.getElementById("tietobtn").style.backgroundColor = "red";
+    tietobtn.style.backgroundColor = "red";
   } else {
     console.log("Tieto 1: " + tieto1.value);
-    console.log("Tieto 2: " + tieto2.value)
+    console.log("Tieto 2: " + tieto2.value);
   }
 }
